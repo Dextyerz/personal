011028e98530702586c7fe48603aaf00f6e35ea9
Dextyerz <88431429+Dextyerz@users.noreply.github.com>
2023-12-28 06:27:32 +0700

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -216,7 +216,7 @@ end
 
 while wait(0.1) do
     PlayerInServer = #Players:GetPlayers()
-    if PlayerInServer < 25 or os.time() >= ostime + 1020 then
+    if PlayerInServer < 25 or os.time() >= ostime + 1080 then
         jumpToServer()
         break
     end
