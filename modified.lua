b0517948cdb3883227f1caa54568161cd2a171c9
Dextyerz <88431429+Dextyerz@users.noreply.github.com>
2023-12-27 18:01:25 +0700

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -47,10 +47,8 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
 
     if string.find(item, "Huge") then
         tag = "<@870106984236609656> NEW HUGE BABY"
-    elseif string.find(item, "Titanic") then
+    elseif string.find(item, "Titanic") and item ~= "Titanic Christmas Present" then
         tag = "@everyone RAWRRR GOT TITANIC BRO"
-    elseif item == "Titanic Christmas Present" then
-        tag = ""
     end
     
     message1 = {
