7ae32d8ccd67514d241d49016cf7757c2c6db8cb
Dextyerz <88431429+Dextyerz@users.noreply.github.com>
2023-12-28 10:06:23 +0700

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,22 +1,28 @@
-import subprocess, time, os;
+import subprocess
+import time
 
-adb = "C:/LDPlayer/LDPlayer9/adb.exe"
+adb = "C:/LDPlayer/LDPlayer4.0/adb.exe"
+place_id = "8737899170"
+
+# Dictionary to keep track of instances and their launch status for a specific placeId
+instance_status = {}
 
 def listADB():
     r = subprocess.run(f"{adb} devices", capture_output=True, text=True)
-    return [line.split('\t')[0] for line in r.stdout.strip().split('\n')[1:]];
+    return [line.split('\t')[0] for line in r.stdout.strip().split('\n')[1:]]
 
 while True:
     instances = listADB()
     for instance in instances:
-        s = subprocess.run(f"{adb} -s {instance} shell pidof com.roblox.client", capture_output=True, text=True)
-        pid = s.stdout
-        if not pid:
-            subprocess.run(f"{adb} -s {instance} shell am start -a android.intent.action.VIEW -d roblox://placeId=8737899170")
-            print(f"launched into game using {instance}");
+        # Check if the instance has been launched for the specific placeId
+        if instance not in instance_status or not instance_status[instance]:
+            s = subprocess.run(f"{adb} -s {instance} shell pidof com.roblox.client", capture_output=True, text=True)
+            pid = s.stdout.strip()
+            if not pid:
+                subprocess.run(f"{adb} -s {instance} shell am start -a android.intent.action.VIEW -d roblox://placeId={place_id}")
+                print(f"Launched into game using {instance}")
+                instance_status[instance] = True  # Mark the instance as launched for the placeId
         else:
-            a = subprocess.run(f"{adb} -s {instance} shell grep pid /data/anr/*com.roblox.client*", capture_output=True, text=True)
-            anr = a.stdout
-            if anr:
-                subprocess.run(f"{adb} -s {instance} shell am force-stop com.roblox.client")
+            # Add additional checks or actions if needed for instances already launched
+
     time.sleep(1)
