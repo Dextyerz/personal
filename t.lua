6fb8b2a94aeeb660fa60f0e6886cc95899644e20
Dextyerz <88431429+Dextyerz@users.noreply.github.com>
2023-12-31 12:43:51 +0700

    Update t.lua

--- t.lua
+++ t.lua
@@ -1,5 +1,5 @@
-getgenv().webhook = "https://discord.com/api/webhooks/1188501479682752615/LCWS4QTpNAG508JKA2osKRtWnYNbem4T43iHJfu4LrCsnyyZrPLvyKq-c5QAJyXeWw7v" -- webhook link for successful snipes
-getgenv().webhookFail = "https://discord.com/api/webhooks/1166673773588647996/adIpnyKJ0sXJ6ZIpzAxJv1KDZJS6ICCrTrcrXj4-RITeIvSUTDNxzZohjJIPjzmS3YD0" -- webhook link for successful snipes
+getgenv().webhook = "https://discord.com/api/webhooks/1188501479682752615/LCWS4QTpNAG508JKA2osKRtWnYNbem4T43iHJfu4LrCsnyyZrPLvyKq-c5QAJyXeWw7v"
+getgenv().webhookFail = "https://discord.com/api/webhooks/1166673773588647996/adIpnyKJ0sXJ6ZIpzAxJv1KDZJS6ICCrTrcrXj4-RITeIvSUTDNxzZohjJIPjzmS3YD0" 
 getgenv().userid = "870106984236609656" -- pings your discord id if it snipes a huge or titanic 
 getgenv().alts = {"Dexalts_mbhrx","Dexalts_9in9e", “Dexalts_a61ng”, “Dexalts_fss4n”, “Dexalts_jhhif”} -- put usernames to hop if more than two of them are in the same server
 getgenv().normalwebhook = ""
