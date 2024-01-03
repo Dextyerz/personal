ef8a314c942de0ebb771dfc8ac0b7f2b636818b3
Dextyerz <88431429+Dextyerz@users.noreply.github.com>
2024-01-03 16:49:56 +0700

    Update t.lua

--- t.lua
+++ t.lua
@@ -1,7 +1,7 @@
 getgenv().webhook = "https://discord.com/api/webhooks/1188501479682752615/LCWS4QTpNAG508JKA2osKRtWnYNbem4T43iHJfu4LrCsnyyZrPLvyKq-c5QAJyXeWw7v"
 getgenv().webhookFail = "https://discord.com/api/webhooks/1166673773588647996/adIpnyKJ0sXJ6ZIpzAxJv1KDZJS6ICCrTrcrXj4-RITeIvSUTDNxzZohjJIPjzmS3YD0" 
 getgenv().userid = "870106984236609656"
-getgenv().alts = {"Dexalts_mbhrx","Dexalts_9in9e", “Dexalts_a61ng”, “Dexalts_fss4n”, “Dexalts_jhhif”} 
+getgenv().alts = {"Dexalts_mbhrx","Dexalts_9in9e", "Dexalts_a61ng", "Dexalts_fss4n", "Dexalts_jhhif"} 
 getgenv().normalwebhook = "https://discord.com/api/webhooks/1166673773588647996/adIpnyKJ0sXJ6ZIpzAxJv1KDZJS6ICCrTrcrXj4-RITeIvSUTDNxzZohjJIPjzmS3YD0"
 getgenv().snipeNormalPets = false 
 repeat wait() until game:IsLoaded()
