9a6fb64a9f70d9136fbe29e94100b2c61391feb9
Dextyerz <88431429+Dextyerz@users.noreply.github.com>
2024-01-03 16:52:02 +0700

    Update t.lua

--- t.lua
+++ t.lua
@@ -3,10 +3,9 @@ getgenv().webhookFail = "https://discord.com/api/webhooks/1166673773588647996/ad
 getgenv().userid = "870106984236609656"
 getgenv().alts = {"Dexalts_mbhrx","Dexalts_9in9e", "Dexalts_a61ng", "Dexalts_fss4n", "Dexalts_jhhif"} 
 getgenv().normalwebhook = "https://discord.com/api/webhooks/1166673773588647996/adIpnyKJ0sXJ6ZIpzAxJv1KDZJS6ICCrTrcrXj4-RITeIvSUTDNxzZohjJIPjzmS3YD0"
-getgenv().snipeNormalPets = false 
 repeat wait() until game:IsLoaded()
 if game.PlaceId == 15502339080 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Root1527/personal/main/modified.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dextyerz/personal/main/modified.lua)()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Root1527/personal/main/silly.lua"))()
 end
